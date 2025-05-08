ca99d61e7ea1c16ca474b5ad970808eb759c6c72
Aji-sudo <115223567+Aji-sudo@users.noreply.github.com>
2025-05-08 12:55:04 +0530

    Create new_sample.py

---
+++ new_sample.py
@@ -0,0 +1,9 @@
+from pyspark.sql import SparkSession
+spark = SparkSession.builder \
+    .appName("S3 to S3 Data Move") \
+    .getOrCreate()
+input_path = "s3a://j-and-j-test-bucket/target_combined_new/part-00000-ce9be0cc-25d3-470b-9745-19c501275b30-c000.snappy.parquet"
+output_path = "s3a://j-and-j-test-bucket/target_combined_new_up"
+df = spark.read.parquet(input_path)
+df.write.mode("overwrite").parquet(output_path)
+spark.stop()
