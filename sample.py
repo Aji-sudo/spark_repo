aba286d550771605182e3502ae0db1393cd0c0f2
Aji-sudo <115223567+Aji-sudo@users.noreply.github.com>
2025-05-08 10:38:11 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -12,42 +12,17 @@
 # df.write.mode("overwrite").parquet(output_path)
  
 # spark.stop()
-
 from pyspark.sql import SparkSession
-
-# Step 1: Initialize Spark session
+ 
 spark = SparkSession.builder \
-    .appName("S3 to Snowflake") \
-    .config("spark.jars.packages", "net.snowflake:spark-snowflake_2.12:2.11.0-spark_3.2,net.snowflake:snowflake-jdbc:3.13.6") \
+    .appName("S3 to S3 Data Move") \
     .getOrCreate()
-
-# Step 2: Define S3 input and Snowflake output paths
-input_path = "s3a://j-and-j-test-bucket/target_combined_new/part-00000-ce9be0cc-25d3-470b-9745-19c501275b30-c000.snappy.parquet"
-output_table = "s3_snowflake_trial_table"  # The target table in Snowflake
-
-# Step 3: Read data from S3 (assuming Parquet format, can change to CSV, JSON, etc.)
+ 
+input_path = "s3a://j-and-j-test-bucket/folder1.parquet/*.parquet"
+output_path = "s3a://j-and-j-test-bucket/target_combined_new_up1/"
+ 
 df = spark.read.parquet(input_path)
-
-# Step 4: Set up Snowflake options
-sfOptions = {
-    "sfURL": "tn30041.europe-west2.gcp.snowflakecomputing.com",
-    "sfUser": "ACME_ADMIN",
-    "sfPassword": "Ajitha_February@2003",  
-    "sfDatabase": "util_db",
-    "sfSchema": "public",
-    "sfWarehouse": "acme_wh"
-}
-
-# Step 5: Write data to Snowflake
-# Step 5: Write data to Snowflake
-df.write \
-    .format("snowflake") \
-    .options(**sfOptions) \
-    .option("dbtable", output_table) \
-    .mode("overwrite") \
-    .save()
-
-
-# Step 6: Stop the Spark session
+ 
+df.coalesce(1).write.mode("overwrite").parquet(output_path)
+ 
 spark.stop()
-
